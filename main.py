9fbdb07e1de1a5e0ae811d637d6960d0f1e08815
mahoo <146084234+okriashvili@users.noreply.github.com>
2025-06-27 15:36:36 +0400

    test commit

--- main.py
+++ main.py
@@ -9,31 +9,31 @@ def text():
     input_text = input("enter a text: ")
     count_upper_case = 0
 
-    # uppercase_count = sum(1 for char in input_text if char.isupper())
-    for char in input_text:
-        if char.isupper():
-            count_upper_case += char.isupper()
-
-    upper_case_input_text = input_text.upper()
-
-    return upper_case_input_text, count_upper_case
-# print(text())
-input_text_in_upper_case, count_upper_case_input_text = text()
-print(f"წინადადებაში,მაღალ რესსტრში გვხვდება {count_upper_case_input_text} ასო")
-print(f"მაღალ რესტრში გადავაქციეთ სიტყვა {input_text_in_upper_case}")
-
-
-# დაწერეთ ფუნქცია, რომელიც პარამეტრად მიიღებს ე.წ. camel case ცვლადებს და დააბრუნებს snake case სახით, ანუ თუ გადავცემთ ცვლადს
-# firstName დააბრუნებს first_name, name დააბრუნებს ისევ name, preferredFirstName დააბრუნებს preferred_first_name, lastName დააბრუნებს
-# last_name და ასე შემდეგ.
+#     # uppercase_count = sum(1 for char in input_text if char.isupper())
+#     for char in input_text:
+#         if char.isupper():
+#             count_upper_case += char.isupper()
+#
+#     upper_case_input_text = input_text.upper()
+#
+#     return upper_case_input_text, count_upper_case
+# # print(text())
+# input_text_in_upper_case, count_upper_case_input_text = text()
+# print(f"წინადადებაში,მაღალ რესსტრში გვხვდება {count_upper_case_input_text} ასო")
+# print(f"მაღალ რესტრში გადავაქციეთ სიტყვა {input_text_in_upper_case}")
+#
+#
+# # დაწერეთ ფუნქცია, რომელიც პარამეტრად მიიღებს ე.წ. camel case ცვლადებს და დააბრუნებს snake case სახით, ანუ თუ გადავცემთ ცვლადს
+# # firstName დააბრუნებს first_name, name დააბრუნებს ისევ name, preferredFirstName დააბრუნებს preferred_first_name, lastName დააბრუნებს
+# # last_name და ასე შემდეგ.
 def camel_to_snake(camel_str):
+    if camel_str[0].isupper():
+        camel_str = camel_str[0].lower() +  camel_str[1:]
     for char in camel_str:
         if char.isupper():
             camel_str = camel_str.replace(char, '_' + char.lower())
     return camel_str
 print(camel_to_snake(input("enter a camel case string: ")))
-
-
 
 
 
